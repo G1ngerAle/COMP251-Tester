7a77449e217464a159753c6b3b38e43c5d7e98e4
Henry Huang <64996660+G1ngerAle@users.noreply.github.com>
2024-12-10 16:53:12 -0500

    Update Jackson's tests and ignore the order in bestMetro tests

--- McMetroTest.java
+++ McMetroTest.java
@@ -25,6 +25,41 @@ class McMetroTest {
     }
 
     @Test
+    void testMaxPassengers1() {
+        // Buildings
+        BuildingID b1 = new BuildingID(1);
+        BuildingID b2 = new BuildingID(2);
+        BuildingID b3 = new BuildingID(3);
+        BuildingID b4 = new BuildingID(4);
+        BuildingID b5 = new BuildingID(5);
+        BuildingID b6 = new BuildingID(6);
+
+        Building[] buildings = new Building[]{
+                new Building(b1, 10),
+                new Building(b2, 10),
+                new Building(b3, 10),
+                new Building(b4, 10),
+                new Building(b5, 10),
+                new Building(b6, 10)
+        };
+
+        // Tracks
+        Track[] tracks = new Track[]{
+                new Track(new TrackID(1), b1, b2, 5, 3),
+                new Track(new TrackID(2), b1, b4, 5, 4),
+                new Track(new TrackID(3), b2, b4, 5, 2),
+                new Track(new TrackID(4), b2, b3, 5, 1),
+                new Track(new TrackID(5), b4, b3, 5, 4),
+                new Track(new TrackID(6), b4, b5, 5, 2),
+                new Track(new TrackID(7), b3, b6, 5, 3),
+                new Track(new TrackID(8), b5, b6, 5, 2)
+        };
+        McMetro metro = new McMetro(tracks, buildings);
+        int maxPassengers = metro.maxPassengers(b1, b6);
+        assertEquals(5, maxPassengers);
+    }
+
+    @Test
     void testMaxPassengers2() {
         // random graph
         BuildingID bid1 = new BuildingID(1);
@@ -663,9 +698,6 @@ class McMetroTest {
 
         McMetro mcMetro = new McMetro(tracks, buildings);
         TrackID[] bms = mcMetro.bestMetroSystem();
-        for (TrackID trackID : bms) {
-            System.out.println(trackID);
-        }
         TrackID[][] expected = {
                 new TrackID[]{tid2, tid3},
                 new TrackID[]{tid3, tid2},
@@ -701,9 +733,6 @@ class McMetroTest {
 
         McMetro mcMetro = new McMetro(tracks, buildings);
         TrackID[] bms = mcMetro.bestMetroSystem();
-        for (TrackID trackID : bms) {
-            System.out.println(trackID);
-        }
         TrackID[][] expected = {
                 new TrackID[]{tid2, tid3, tid4},
                 new TrackID[]{tid2, tid, tid4},
@@ -742,11 +771,12 @@ class McMetroTest {
 
         McMetro mcMetro = new McMetro(tracks, buildings);
         TrackID[] bms = mcMetro.bestMetroSystem();
-        for (TrackID trackID : bms) {
-            System.out.println(trackID);
-        }
-        TrackID[] expected = new TrackID[] {tid2, tid}; // if your printed output has the same tid, you are good
-        assertArrayEquals(expected, bms);
+        TrackID[] expected = new TrackID[] {tid2, tid3}; // if your printed output has the same tid, you are good
+        Arrays.sort(bms);
+        Arrays.sort(expected);
+
+        // check if arrays are equal ignoring order
+        assertTrue(Arrays.equals(expected, bms));
     }
 
     @Test
@@ -761,9 +791,6 @@ class McMetroTest {
 
         McMetro mcMetro = new McMetro(tracks, buildings);
         TrackID[] bms = mcMetro.bestMetroSystem();
-        for (TrackID trackID : bms) {
-            System.out.println(trackID);
-        }
         TrackID[] expected = new TrackID[]{}; // if your printed output has the same tid, you are good
         assertArrayEquals(expected, bms);
     }
@@ -822,11 +849,12 @@ class McMetroTest {
 
         McMetro mcMetro = new McMetro(tracks, buildings);
         TrackID[] bms = mcMetro.bestMetroSystem();
-        for (TrackID trackID : bms) {
-            System.out.println(trackID);
-        }
         TrackID[] expected = new TrackID[]{tid, tid4, tid7}; // if your printed output has the same tid, you are good
-        assertArrayEquals(expected, bms);
+        Arrays.sort(bms);
+        Arrays.sort(expected);
+
+        // check if arrays are equal ignoring order
+        assertTrue(Arrays.equals(expected, bms));
     }
 
     @Test
@@ -864,10 +892,65 @@ class McMetroTest {
         };
         McMetro mcMetro = new McMetro(tracks, buildings);
         TrackID[] bms = mcMetro.bestMetroSystem();
-        for (TrackID trackID : bms) {
-            System.out.println(trackID);
-        }
         TrackID[] expected = new TrackID[]{tid7, tid6, tid3, tid5, tid2}; // if your printed output has the same tid, you are good
+        Arrays.sort(bms);
+        Arrays.sort(expected);
+
+        // check if arrays are equal ignoring order
+        assertTrue(Arrays.equals(expected, bms));
+    }
+
+    @Test
+    void testBestMetro8() {
+        // Buildings
+        BuildingID b1 = new BuildingID(1);
+        BuildingID b2 = new BuildingID(2);
+        BuildingID b3 = new BuildingID(3);
+        BuildingID b4 = new BuildingID(4);
+        BuildingID b5 = new BuildingID(5);
+        BuildingID b6 = new BuildingID(6);
+
+        Building[] buildings = new Building[]{
+                new Building(b1, 10),
+                new Building(b2, 10),
+                new Building(b3, 10),
+                new Building(b4, 10),
+                new Building(b5, 10),
+                new Building(b6, 10)
+        };
+
+        // Tracks
+        TrackID t1 = new TrackID(1);
+        TrackID t2 = new TrackID(2);
+        TrackID t3 = new TrackID(3);
+        TrackID t4 = new TrackID(4);
+        TrackID t5 = new TrackID(5);
+        TrackID t6 = new TrackID(6);
+        TrackID t7 = new TrackID(7);
+        TrackID t8 = new TrackID(8);
+        Track[] tracks = new Track[]{
+                new Track(t1, b1, b2, 8, 5),
+                new Track(t2, b1, b4, 7, 5),
+                new Track(t3, b2, b4, 6, 5),
+                new Track(t4, b2, b3, 1, 5),
+                new Track(t5, b4, b3, 4, 5),
+                new Track(t6, b4, b5, 3, 5),
+                new Track(t7, b3, b6, 2, 5),
+                new Track(t8, b5, b6, 5, 5)
+        };
+        McMetro metro = new McMetro(tracks, buildings);
+        TrackID[] bms = metro.bestMetroSystem();
+        TrackID[] expected = new TrackID[]{t4, t7, t5, t8, t2};
+        String result1 = "";
+        String result2 = "";
+        for (int i=0; i<bms.length; i++){
+            result1 += "["+bms[i]+"], ";
+        }
+        for (int i=0; i<expected.length; i++){
+            result2 += "["+expected[i]+"], ";
+        }
+        System.out.println("Expected: "+result2);
+        System.out.println("Actual: "+result1);
         assertArrayEquals(expected, bms);
     }
 
@@ -891,6 +974,89 @@ class McMetroTest {
         mcMetro.addPassengers(passengers);
 
         ArrayList<String> found = mcMetro.searchForPassengers("al");
+
+        assertArrayEquals(expected, found.toArray(new String[0]));
+    }
+
+    @Test
+    void testSearchForPassengers3() {
+        McMetro mcMetro = new McMetro(new Track[0], new Building[0]);
+        String[] passengers = {
+                "Alex", "Bob", "Ally", "al", "Bobby","bObbert", "David",
+                "Davie", "Davis"};
+        String[] expected = {"Al", "Alex", "Ally"};
+        mcMetro.addPassengers(passengers);
+
+        ArrayList<String> found = mcMetro.searchForPassengers("al");
+
+        //printing arrays to compare
+        String actualResult ="";
+        for(int i=0; i<found.size(); i++){
+            actualResult += found.get(i)+", ";
+        }
+        String expectedResult ="";
+        for(int i=0; i<expected.length; i++){
+            expectedResult += expected[i]+", ";
+        }
+        System.out.println("Extepted: " + expectedResult);
+        System.out.println("Actual: " + actualResult);
+
+        assertArrayEquals(expected, found.toArray(new String[0]));
+    }
+    @Test
+    void testSearchForPassengers4() {
+        McMetro mcMetro = new McMetro(new Track[0], new Building[0]);
+        String[] passengers = {
+                "Alex", "Bob", "Ally", "al", "Bobby","bObbert", "David",
+                "Davie", "Davis",};
+        String[] expected = {"Bob", "Bobbert", "Bobby"};
+        mcMetro.addPassengers(passengers);
+        String[] found = mcMetro.searchForPassengers("bob").toArray(new String[0]);
+        Arrays.sort(found);
+        Arrays.sort(expected);
+
+        // check if arrays are equal ignoring order
+        assertTrue(Arrays.equals(expected, found));
+
+    }
+    @Test
+    void testSearchForPassengers5() {
+        McMetro mcMetro = new McMetro(new Track[0], new Building[0]);
+        String[] passengers = {
+                "Alex", "Bob", "Ally", "al", "Bobby","bObbert", "David",
+                "Davie", "Davis",};
+        String[] expected = {"Davis", "David", "Davie"};
+        mcMetro.addPassengers(passengers);
+
+        String[] found = mcMetro.searchForPassengers("dav").toArray(new String[0]);
+        Arrays.sort(found);
+        Arrays.sort(expected);
+
+        // check if arrays are equal ignoring order
+        assertTrue(Arrays.equals(expected, found));
+    }
+    @Test
+    void testSearchForPassengers6() {
+        McMetro mcMetro = new McMetro(new Track[0], new Building[0]);
+        String[] passengers = {
+                "Alex", "Bob", "Ally", "al", "Bobby","bObbert", "David",
+                "Davie", "Davis",};
+        String[] expected = {};
+        mcMetro.addPassengers(passengers);
+
+        ArrayList<String> found = mcMetro.searchForPassengers("col");
+
+        //printing arrays to compare
+        String actualResult ="";
+        for(int i=0; i<found.size(); i++){
+            actualResult += found.get(i)+", ";
+        }
+        String expectedResult ="";
+        for(int i=0; i<expected.length; i++){
+            expectedResult += expected[i]+", ";
+        }
+        System.out.println("Extepted: " + expectedResult);
+        System.out.println("Actual: " + actualResult);
 
         assertArrayEquals(expected, found.toArray(new String[0]));
     }
@@ -969,5 +1135,34 @@ class McMetroTest {
 
         int toHire = McMetro.hireTicketCheckers(schedule);
         assertEquals(4, toHire);
+    }
+
+    @Test
+    void testHireTicketCheckers5() {
+        int[][] schedule = new int[][]{
+                {1,2}, {2,3}, {3,4}, {1,3},
+                {1,4}, {3,6}, {4,7}, {4,5},
+                {5,6}, {4,8}, {2,4}, {6,7}
+        };
+
+        int toHire = McMetro.hireTicketCheckers(schedule);
+        assertEquals(6, toHire);
+    }
+
+    @Test
+    void testHireTicketCheckers6(){
+        int[][] schedule = new int[][]{
+                {0,2}, {1,3}, {2,5}, {4,6},
+                {5,9}, {6,9}, {8,10}
+        };
+
+        int toHire = McMetro.hireTicketCheckers(schedule);
+        assertEquals(3, toHire);
+    }
+    @Test
+    void testHireTicketCheckers7(){
+        int[][] schedule = new int[][]{{1,3}, {1,3}, {1,3}};
+        int toHire = McMetro.hireTicketCheckers(schedule);
+        assertEquals(1, toHire);
     }
 }
